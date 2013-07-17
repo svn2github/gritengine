119d1c598c3dbcbca07f2aca15ba6d4394466e72
paroxum <paroxum@3c9ba142-2be6-4198-8de6-d73c80d60e27>
2013-07-17 20:31:21 +0000

    !A Added two new platform sizes to whitebox, started to build the outer ring with street variations
    git-svn-id: http://svn.code.sf.net/p/gritengine/code/trunk@2695 3c9ba142-2be6-4198-8de6-d73c80d60e27

--- grit_core/media/detached/map/whitebox_definitions.lua
+++ grit_core/media/detached/map/whitebox_definitions.lua
@@ -5,6 +5,8 @@ material "water" { diffuseColour= {.2,.5,1}; }
 
 class "citadel_wb" (ColClass) {renderingDistance=600}
 class "static_ground_wb" (ColClass) {renderingDistance=600}
+class "static_ground_wb2" (ColClass) {renderingDistance=600}
+class "static_ground_wb3" (ColClass) {renderingDistance=600}
 class "static_ground_angle_wb" (ColClass) {renderingDistance=600}
 class "building_block_large" (ColClass) {renderingDistance=600}
 class "building_block_med" (ColClass) {renderingDistance=600}
