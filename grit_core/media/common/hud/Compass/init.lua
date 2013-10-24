de74daa31e07c20f8c16895336911c359d3101f8
sparksparkspark <sparksparkspark@3c9ba142-2be6-4198-8de6-d73c80d60e27>
2013-10-24 14:16:36 +0000

    Add inheritOrientation, windows build, refactor compass
    git-svn-id: http://svn.code.sf.net/p/gritengine/code/trunk@2804 3c9ba142-2be6-4198-8de6-d73c80d60e27

--- grit_core/media/common/hud/Compass/init.lua
+++ grit_core/media/common/hud/Compass/init.lua
@@ -8,12 +8,19 @@ hud_class "../Compass" {
 
         self.texture = "Compass/Body.png"
 		
-        --pointer denotes a camera direction on the compass
-        self.pointer = gfx_hud_object_add("Rect", {texture = "Compass/Pointer.png", zOrder = 7, size=self.size})
+        --pointer is the notch at the top of the compass that marks where you're looking
+        self.pointer = gfx_hud_object_add("Rect", {texture = "Compass/Pointer.png", zOrder = 7})
         self.pointer.parent = self
-        --needle denotes the player model direction (if any) or same as the cam direction
-        self.needle = gfx_hud_object_add("Rect", {texture = "Compass/Needle.png", size=self.size})
-        self.needle.parent = self
+        self.pointer.position = vec(0, self.size.y/2-24)
+
+        -- compass orientation (what makes N point towards north) 
+        self.ring = gfx_hud_object_add("Rect", {alpha=0, size=self.size})
+        self.ring.parent = self
+
+        --player denotes the player model direction (if any)
+        self.player = gfx_hud_object_add("Rect", {texture = "Compass/Needle.png"})
+        self.player.parent = self.ring
+
 
         --bearing displayed in degrees
         self.text = gfx_hud_text_add("TinyFont")
@@ -22,37 +29,42 @@ hud_class "../Compass" {
         self.text.position = vector2(48,-50)
         self.text.colour = vector3(0,0,0)
 
-        self.letterInset = vector2(2, 2)
+        local inner_sz = self.size - vec(20,20)
 
         --the cardinal directions
         local cardinalLettersFont = "/system/misc.fixed"
         self.N = gfx_hud_text_add(cardinalLettersFont)
         self.N.text = "N"
-        self.N.parent = self
-        self.N.position = vector2(0, self.size.y/2-self.N.size.y/2-self.letterInset.y)
+        self.N.parent = self.ring
         self.N.colour = vector3(1,0,0)
+        self.N.position = inner_sz * vec (0,0.5)
+        self.N.inheritOrientation = false
 
         self.S = gfx_hud_text_add(cardinalLettersFont)
         self.S.text = "S"
-        self.S.parent = self
-        self.S.position = vector2(0, -(self.size.y/2-self.S.size.y/2-self.letterInset.y))
+        self.S.parent = self.ring
         self.S.colour = vector3(0,0,0)
+        self.S.position = inner_sz * vec (0,-0.5)
+        self.S.inheritOrientation = false
 
         self.W = gfx_hud_text_add(cardinalLettersFont)
         self.W.text = "W"
-        self.W.parent = self
-        self.W.position = vector2(-(self.size.x/2-self.W.size.x/2-self.letterInset.x), 0)
+        self.W.parent = self.ring
         self.W.colour = vector3(0,0,0)
+        self.W.position = inner_sz * vec (-0.5,0)
+        self.W.inheritOrientation = false
 
         self.E = gfx_hud_text_add(cardinalLettersFont)
         self.E.text = "E"
-        self.E.parent = self
-        self.E.position = vector2(self.size.x/2-self.W.size.x-self.letterInset.x, 0)
+        self.E.parent = self.ring
         self.E.colour = vector3(0,0,0)
+        self.E.position = inner_sz * vec (0.5,0)
+        self.E.inheritOrientation = false
     end;
     destroy = function (self)
         safe_destroy(self.pointer)
-        safe_destroy(self.needle)
+        safe_destroy(self.ring)
+        safe_destroy(self.player)
         safe_destroy(self.text)
         safe_destroy(self.N)
         safe_destroy(self.S)
@@ -62,36 +74,15 @@ hud_class "../Compass" {
     frameCallback = function (self, elapsed)
         local orientation = -player_ctrl.camYaw
         local o = player_ctrl.controlObj
-        if o then
-            if o and o.instance and o.instance.body then
-                local v = o.instance.body.worldOrientation*V_FORWARDS
-                self.needle.orientation = math.deg(math.atan2(v.x, v.y))+orientation
-            else
-                self.needle.orientation = 0
-            end
+        if o and o.instance and o.instance.body then
+            self.player.enabled = true
+            local v = o.instance.body.worldOrientation*V_FORWARDS
+            self.player.orientation = math.deg(math.atan2(v.x, v.y))
         else
-            self.needle.orientation = 0
+            self.player.enabled = false
         end
         self.text.text = string.format("%03d",player_ctrl.camYaw + 0.5)
-        local radOrientation = math.rad(orientation)
-        local s, c = math.sin(radOrientation), math.cos(radOrientation)
-        self.N.position = vector2(s*(self.size.x/2-self.N.size.x/2-self.letterInset.x),
-                                  c*(self.size.y/2-self.N.size.y/2-self.letterInset.y))
-
-        radOrientation = math.rad(orientation+180)
-        s, c = math.sin(radOrientation), math.cos(radOrientation)
-        self.S.position = vector2(s*(self.size.x/2-self.S.size.x/2-self.letterInset.x),
-                                  c*(self.size.y/2-self.S.size.y/2-self.letterInset.y))
-
-        radOrientation = math.rad(orientation+270)
-        s, c = math.sin(radOrientation), math.cos(radOrientation)
-        self.W.position = vector2(s*(self.size.x/2-self.W.size.x/2-self.letterInset.x),
-                                  c*(self.size.y/2-self.W.size.y/2-self.letterInset.y))
-
-        radOrientation = math.rad(orientation+90)
-        s, c = math.sin(radOrientation), math.cos(radOrientation)
-        self.E.position = vector2(s*(self.size.x/2-self.E.size.x/2-self.letterInset.x),
-                                  c*(self.size.y/2-self.E.size.y/2-self.letterInset.y)) 
+        self.ring.orientation = orientation
     end;
 }
 
