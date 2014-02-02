9c8f63fbfb480c6eb506842a5d4199539acc1dbc
mortalhuman <mortalhuman@3c9ba142-2be6-4198-8de6-d73c80d60e27>
2014-02-02 22:34:29 +0000

    apply alpha reject
    git-svn-id: http://svn.code.sf.net/p/gritengine/code/trunk@2956 3c9ba142-2be6-4198-8de6-d73c80d60e27

--- grit_core/media/top_gear/materials.lua
+++ grit_core/media/top_gear/materials.lua
@@ -5,4 +5,5 @@ material "grass01" { diffuseMap="grass_d.dds" }
 material "concrete" { diffuseMap="concrete.dds" }
 material "grass_mowed" { diffuseMap="grass_mowed.dds" }
 material "grass_shaggy" { diffuseMap="grass_shaggy.dds" }
-material "grass_glue_mowshag" { diffuseMap="grass_glue_mowshag.dds", alpha = 1.0}
+--material "grass_glue_mowshag" { diffuseMap="grass_glue_mowshag.dds", alpha = 1.0}
+material "grass_glue_mowshag" { diffuseMap="grass_glue_mowshag.dds", alphaReject=0.5, castShadows=false}
