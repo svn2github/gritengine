1e5cbfc616860538cc6ed409cbb7557f70fae7dd
mortalhuman <mortalhuman@3c9ba142-2be6-4198-8de6-d73c80d60e27>
2014-02-02 05:00:00 +0000

    quick example of different grasses blended alpha texture, need to disable ground from casting shadows. How?
    git-svn-id: http://svn.code.sf.net/p/gritengine/code/trunk@2952 3c9ba142-2be6-4198-8de6-d73c80d60e27

--- grit_core/media/top_gear/materials.lua
+++ grit_core/media/top_gear/materials.lua
@@ -3,3 +3,6 @@ material "raceway_main" { diffuseMap="raceway_main.png" }
 material "tiled_pavement" { diffuseMap="tiled_pavement.png" }
 material "grass01" { diffuseMap="grass_d.dds" }
 material "concrete" { diffuseMap="concrete.png" }
+material "grass_mowed" { diffuseMap="grass_mowed.dds" }
+material "grass_shaggy" { diffuseMap="grass_shaggy.dds" }
+material "grass_glue_mowshag" { diffuseMap="grass_glue_mowshag.dds", alpha = 1.0}
