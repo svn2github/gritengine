9bdf12f5cdb0754de9f0fc73ed929468a050e35e
mortalhuman <mortalhuman@3c9ba142-2be6-4198-8de6-d73c80d60e27>
2014-02-02 06:34:52 +0000

    drop pngs for dds
    git-svn-id: http://svn.code.sf.net/p/gritengine/code/trunk@2954 3c9ba142-2be6-4198-8de6-d73c80d60e27

--- grit_core/media/top_gear/materials.lua
+++ grit_core/media/top_gear/materials.lua
@@ -1,8 +1,8 @@
 material "Track" { diffuseMap="Track.dds", shadowBias = 0.1 }
-material "raceway_main" { diffuseMap="raceway_main.png" }
-material "tiled_pavement" { diffuseMap="tiled_pavement.png" }
+material "raceway_main" { diffuseMap="raceway_main.dds" }
+material "tiled_pavement" { diffuseMap="tiled_pavement.dds" }
 material "grass01" { diffuseMap="grass_d.dds" }
-material "concrete" { diffuseMap="concrete.png" }
+material "concrete" { diffuseMap="concrete.dds" }
 material "grass_mowed" { diffuseMap="grass_mowed.dds" }
 material "grass_shaggy" { diffuseMap="grass_shaggy.dds" }
 material "grass_glue_mowshag" { diffuseMap="grass_glue_mowshag.dds", alpha = 1.0}
