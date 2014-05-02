1ec680a06153ea43b47fa6a13a5e6c0c01c177eb
sparksparkspark <sparksparkspark@3c9ba142-2be6-4198-8de6-d73c80d60e27>
2014-05-02 02:15:20 +0000

    Revamp the editbox.  Give it a caret.  Allow clicking to move the caret around.  Remove BorderPane (no-longer used).
    git-svn-id: http://svn.code.sf.net/p/gritengine/code/trunk@3071 3c9ba142-2be6-4198-8de6-d73c80d60e27

--- grit_core/media/common/hud/base.lua
+++ grit_core/media/common/hud/base.lua
@@ -2,48 +2,6 @@
 
 hud_class `Rect` { }
 
-BorderPane = {
-    borderColour = vec(0, 0, 0);
-    colour = vec(1, 1, 1);
-    alpha = 1;
-    init = function (self)
-        self.top    = gfx_hud_object_add(`/common/hud/Rect`, {parent=self})
-        self.bottom = gfx_hud_object_add(`/common/hud/Rect`, {parent=self})
-        self.left   = gfx_hud_object_add(`/common/hud/Rect`, {parent=self})
-        self.right  = gfx_hud_object_add(`/common/hud/Rect`, {parent=self})
-        self:setBorderColour(self.borderColour)
-        BorderPane.updateChildrenSize(self)
-    end;
-
-    updateChildrenSize = function (self)
-        local sz = self.size
-
-        self.top.size = vec(sz.x, 1)
-        self.top.position = vec(0, -sz.y/2 + 0.5)
-
-        self.bottom.size = vec(sz.x, 1)
-        self.bottom.position = vec(0, sz.y/2 - 0.5)
-
-        self.left.size = vec(1, sz.y)
-        self.left.position = vec(-sz.x/2 + 0.5, 0)
-
-        self.right.size = vec(1, sz.y)
-        self.right.position = vec(sz.x/2 - 0.5, 0)
-    end;
-
-    setBorderColour = function (self, colour)
-        self.top.colour = colour
-        self.bottom.colour = colour
-        self.left.colour = colour
-        self.right.colour = colour
-        self.borderColour = colour
-    end;
-
-    destroy = function (self)
-    end;
-}
-
-
 hud_class `Positioner` {     
     size = vec(0, 0);    
     factor = vec(1, 1);
--- grit_core/media/common/hud/editbox.lua
+++ grit_core/media/common/hud/editbox.lua
@@ -1,41 +1,78 @@
 -- (c) David Cunningham 2014, Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+
+-- What character index corresponds to the given pixel position?
+-- Returns between 0 and #text inclusive
+function text_char_pos (font, text, x)
+    local len = #text
+    local last_char_pos = 0
+    for i=1, len do
+        local this_char_pos = gfx_font_text_width(font, text:sub(1, i))
+        if this_char_pos > x then
+            if math.abs(last_char_pos - x) < math.abs(this_char_pos - x) then
+                return i - 1
+            else
+                return i
+            end
+        end
+        last_char_pos = this_char_pos
+    end
+    return len
+end
+
 -- Value is always text.  Can set .number to restrict input to numbers, but 
 -- empty string is still a valid state. If using .number, use tonumber(blah.value) or 0
 
-hud_class `EditBox` (extends (BorderPane) {
+hud_class `EditBox` {
 
-    textColour = vec(0, 0, 0);
-    selectedBorderColour = vec(1, 0, 0);
+    textColour = vec(1, 1, 1);
+    borderColour = 0.4 * vec(1, 1, 1);
+    padding = 4;
+    colour = 0.25 * vec(1, 1, 1);
+    texture = `CornerTextures/Filled02.png`;
+    cornered = true;
     font = `/common/fonts/Verdana12`;
     value = "Text";
     number = false;
     alignment = "CENTER";
 
     init = function (self)
-        BorderPane.init(self)
-        
-        self.originalBorderColour = self.borderColour
 
-        self.value = self.value
 
         self.needsInputCallbacks = true
         self.text = gfx_hud_text_add(self.font)
-        self.text.colour = self.textColour
         self.text.parent = self
-        self.text.text = self.value
+
+        self.border = gfx_hud_object_add(`Rect`, {
+            texture = `CornerTextures/Border02.png`;
+            colour = self.borderColour;
+            cornered = true;
+            parent = self;
+        })
+
+        self.caret = gfx_hud_object_add(`Rect`, {
+            texture = `CornerTextures/Caret.png`;
+            colour = self.textColour;
+            cornered = true;
+            parent = self;
+        })
 
         self.inside = false
-        self.editting = false
-        self.anim = 0
-        if self.greyed == nil then self.greyed = false end
+        self:setGreyed(not not self.greyed)
+        self:setEditting(false, true)
 
+        self:setValue(self.value)
+    end;
+
+    updateText = function (self)
+        self.text.text = self.before .. self.after
         self:updateChildrenSize()
+        self.value = self.before .. self.after
     end;
     
-    setGreyed = function (self, v)
+    setGreyed = function (self, v, no_callback)
         if v then
             self.text.colour = vec(0.5, 0.5, 0.5)
-            self:setEditting(false)
+            self:setEditting(false, no_callback)
         else
             self.text.colour = self.textColour
         end
@@ -46,36 +83,61 @@ hud_class `EditBox` (extends (BorderPane) {
     end;
 
     mouseMoveCallback = function (self, local_pos, screen_pos, inside)
-        self.inside = inside
+        self.inside = inside and local_pos
     end;
 
     onEditting = function (self, editting)
     end;
     
-    setEditting = function (self, editting)
+    setEditting = function (self, editting, no_callback)
         if self.editting == editting then return end
         if self.greyed then return end
         self.editting = editting
-        if editting then
-            self.anim = 0
-            self.needsFrameCallbacks = true
-        else
-            self:setBorderColour(self.originalBorderColour)
-            self.needsFrameCallbacks = false
+        self.caret.enabled = editting
+        self.needsFrameCallbacks = editting
+        if not no_callback then
+            self:onEditting(editting)
         end
-        self:onEditting(editting)
     end;
 
     buttonCallback = function (self, ev)
         if self.greyed then return end
         if ev == "+left" then
-            self:setEditting(self.inside)
+            if self.inside == false then
+                self:setEditting(false)
+            else
+                self:setEditting(true)
+                local txt = self.value
+                local pos = text_char_pos(self.font, txt, self.inside.x + self.size.x/2 - self.padding)
+                self.before = txt:sub(1, pos)
+                self.after = txt:sub(pos+1)
+                self:updateText()
+            end
         elseif self.editting then
             if ev == "+Return" then
                 self:setEditting(false)
             elseif ev == "+BackSpace" or ev == "=BackSpace" then
-                self:setValue(self.value:sub(1,-2))
+                self.before = self.before:sub(1, -2)
+                self:updateText()
                 self:onChange(self)
+            elseif ev == "+Delete" or ev == "=Delete" then
+                self.after = self.after:sub(2)
+                self:updateText()
+                self:onChange(self)
+            elseif ev == "+Left" or ev == "=Left" then
+                if #self.before > 0 then
+                    local char = self.before:sub(-1, -1)
+                    self.before = self.before:sub(1, -2)
+                    self.after = char .. self.after
+                    self:updateText()
+                end
+            elseif ev == "+Right" or ev == "=Right" then
+                if #self.after > 0 then
+                    local char = self.after:sub(1, 1)
+                    self.after = self.after:sub(2)
+                    self.before = self.before .. char
+                    self:updateText()
+                end
             elseif ev:sub(1,1) == ":" then
                 if self.maxLength == nil or #self.value < self.maxLength then
                     local key = ev:sub(2)
@@ -84,7 +146,8 @@ hud_class `EditBox` (extends (BorderPane) {
                        key:match("[0-9]") or
                        (key == "." and not text:match("[.]"))
                     then
-                        self:setValue(text .. ev:sub(2))
+                        self.before = self.before .. ev:sub(2)
+                        self:updateText()
                         self:onChange(self)
                     end
                 end
@@ -92,21 +155,23 @@ hud_class `EditBox` (extends (BorderPane) {
         end
     end;
     
-    frameCallback = function (self, elapsed)
-        self.anim = ( self.anim + elapsed ) % 1
-        local alpha = math.pow(math.sin(self.anim * 2 * math.pi)/2+0.5,1/2.2)
-        self:setBorderColour(lerp(self.originalBorderColour, self.selectedBorderColour, alpha))
+    frameCallback = function (self)
+        local state = (seconds() % 0.5) / 0.5
+        self.caret.enabled = state < 0.66
     end;
 
     updateChildrenSize = function (self)
-        BorderPane.updateChildrenSize(self)
         if self.alignment == "CENTRE" then
             self.text.position = vec(0,0)
         elseif self.alignment == "LEFT" then
-            self.text.position = - vec(self.size.x/2 -4 - self.text.size.x/2, 0)
+            self.text.position = - vec(self.size.x/2 - self.padding - self.text.size.x/2, 0)
         elseif self.alignment == "RIGHT" then
-            self.text.position = vec(self.size.x/2 -4 - self.text.size.x/2, 0)
+            self.text.position = vec(self.size.x/2 - self.padding - self.text.size.x/2, 0)
         end
+        self.caret.size = vec(self.caret.size.x, self.size.y-4)
+        self.border.size = self.size
+        local tw = gfx_font_text_width(self.font, self.before)
+        self.caret.position = vec(self.text.position.x -self.text.size.x/2 + tw, 0)
     end;
     
     onChange = function (self)
@@ -114,9 +179,10 @@ hud_class `EditBox` (extends (BorderPane) {
     end;
     
     setValue = function (self, value)
-        self.value = value
-        self.text.text = self.value
-        self:updateChildrenSize()
+        self.before = value
+        self.after = ""
+        self:updateText()
     end;
 
-})
+}
+
