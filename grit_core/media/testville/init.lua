e2ab28376da890afbee4b73f7411410e82c9a880
sparksparkspark <sparksparkspark@3c9ba142-2be6-4198-8de6-d73c80d60e27>
2014-05-11 00:43:19 +0000

    testville has been broken for a while, fixing it
    git-svn-id: http://svn.code.sf.net/p/gritengine/code/trunk@3096 3c9ba142-2be6-4198-8de6-d73c80d60e27

--- grit_core/media/testville/init.lua
+++ grit_core/media/testville/init.lua
@@ -1,7 +1,7 @@
 -- (c) David Cunningham 2009, Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 
-procedural_batch "GrassMesh" {
-    mesh = "/common/veg/GrassMesh.mesh",
+procedural_batch `GrassMesh` {
+    mesh = `/common/veg/GrassMesh.mesh`,
     density = .2,
     minSlope = 0, maxSlope = 50,
     rotate = true,
@@ -12,8 +12,8 @@ procedural_batch "GrassMesh" {
     triangles = 15000,
 }
 
-procedural_batch "TropPlant1" {
-    mesh = "/common/veg/TropPlant1.mesh",
+procedural_batch `TropPlant1` {
+    mesh = `/common/veg/TropPlant1.mesh`,
     density = .01,
     minSlope = 0, maxSlope = 50,
     rotate = true,
@@ -25,8 +25,8 @@ procedural_batch "TropPlant1" {
     triangles = 10000,
 }
 
-procedural_batch "Stone" {
-    mesh = "/common/props/nature/rock.mesh",
+procedural_batch `Stone` {
+    mesh = `/common/props/nature/rock.mesh`,
     density = .01,
     minSlope = 0, maxSlope = 50,
     rotate = true,
@@ -38,39 +38,39 @@ procedural_batch "Stone" {
     triangles = 10000,
 }
 
-physical_material "Floor" {
+physical_material `Floor` {
     interactionGroup = DeformGroup,
     roadTyreFriction = 0.5,
     offRoadTyreFriction = 1.5,
-    --proceduralBatches = { "GrassMesh" }
-    --proceduralBatches = { "TropPlant1", "Stone" }
+    --proceduralBatches = { `GrassMesh` }
+    --proceduralBatches = { `TropPlant1`, `Stone` }
 }
 
-material "RoadMarkings" { }
+material `RoadMarkings` { }
 
-material "Floor" { diffuseMap="/common/tex/Test.dds", textureScale={.25,.25} }
---material "Floor" { }
+material `Floor` { diffuseMap=`/common/tex/Test.dds`, textureScale={.25,.25} }
+--material `Floor` { }
 
---material "RoadSurface" { diffuseMap="RoadSurface_d.dds", normalMap="RoadSurface_n.dds", specularMap="RoadSurface_s.dds" }
---material "RoadSurface" { diffuseMap="BreezeBlocks_d.dds", normalMap="BreezeBlocks_n.dds" }
-material "RoadSurface" { diffuseMap="RedBrickVert_d.dds", normalMap="RedBrickVert_n.dds" }
-material "RoofTiles" { diffuseMap="FeltTiles_d.png", specularFromDiffuse={-1,1}, diffuseColour=srgb(248, 253, 255), textureScale={.25,.25} }
---material "RoadSurface" { diffuseMap="RedBrick_d.dds", normalMap="RedBrick_n.dds" }
---material "RoadMarkings" { diffuseMap="RoadMarkings_d.dds", normalMap="RoadMarkings_n.dds", specularMap="RoadMarkings_s.dds" }
-material "RoadMarkings" { overlayOffset=true, alphaReject=0.5, premultipliedAlpha=true, diffuseMap="RoadMarkings_d.dds", normalMap="RoadMarkings_n.dds", specularMap="RoadMarkings_s.dds" }
-class "TestFloor"   (ColClass) {receiveShadows=true,renderingDistance=1200,castShadows=true}
-class "Floor"       (ColClass) {receiveShadows=true,renderingDistance=1200,castShadows=true}
-class "FloorMarkings" (BaseClass) {gfxMesh=`Floor.mesh`, receiveShadows=true,renderingDistance=1200, materialMap={RoadSurface=`RoofTiles`}}
-class "ThunderDome" (ColClass) {receiveShadows=true,renderingDistance=400,castShadows=true}
+--material `RoadSurface` { diffuseMap=`RoadSurface_d.dds`, normalMap=`RoadSurface_n.dds`, specularMap=`RoadSurface_s.dds` }
+--material `RoadSurface` { diffuseMap=`BreezeBlocks_d.dds`, normalMap=`BreezeBlocks_n.dds` }
+material `RoadSurface` { diffuseMap=`RedBrickVert_d.dds`, normalMap=`RedBrick_n.dds` }
+material `RoofTiles` { diffuseMap=`/common/tex/davec/RoofTiles_d.dds`, diffuseColour=srgb(248, 253, 255), textureScale={.25,.25} }
+--material `RoadSurface` { diffuseMap=`RedBrick_d.dds`, normalMap=`RedBrick_n.dds` }
+--material `RoadMarkings` { diffuseMap=`RoadMarkings_d.dds`, normalMap=`RoadMarkings_n.dds`, specularMap=`RoadMarkings_s.dds` }
+material `RoadMarkings` { overlayOffset=true, alphaReject=0.5, premultipliedAlpha=true, diffuseMap=`RoadMarkings_d.dds`, normalMap=`RoadMarkings_n.dds`, specularMap=`RoadMarkings_s.dds` }
+class `TestFloor`   (ColClass) {receiveShadows=true,renderingDistance=1200,castShadows=true}
+class `Floor`       (ColClass) {receiveShadows=true,renderingDistance=1200,castShadows=true}
+class `FloorMarkings` (BaseClass) {gfxMesh=`Floor.mesh`, receiveShadows=true,renderingDistance=1200, materialMap={[`RoadSurface`]=`RoofTiles`}}
+class `ThunderDome` (ColClass) {receiveShadows=true,renderingDistance=400,castShadows=true}
 
 
-object "Floor"       (0,0,0)   {rot=quat(1,0,0,0), name="Floor", materialMap={RoadSurface=`RoofTiles`}}
---object "FloorMarkings"       (0,0,0)   {rot=quat(1,0,0,0), name="Markings"}
-object "TestFloor"   (100,0,1)   {rot=quat(1,0,0,0), name="TestFloor"}
+object `Floor`       (0,0,0)   {rot=quat(1,0,0,0), name=`Floor`, materialMap={[`RoadSurface`]=`RoofTiles`}}
+--object `FloorMarkings`       (0,0,0)   {rot=quat(1,0,0,0), name=`Markings`}
+object `TestFloor`   (100,0,1)   {rot=quat(1,0,0,0), name=`TestFloor`}
 
-object "/common/props/street/Lamp" (0,0,3) { name="lamp" }
-object "/vehicles/Evo" (0.5,-4,0.4) { name="evo" }
-object "/common/props/furniture/WorkBench" (-1.761258,-0.358200,0.650000) {name="Unnamed:/common/props/furniture/WorkBench:0", rot=quat(-0.9866859, 0, 0, -0.1626372)}
+object `/common/props/street/Lamp` (0,0,3) { name=`lamp` }
+object `/vehicles/Evo` (0.5,-4,0.4) { name=`evo` }
+object `/common/props/furniture/WorkBench` (-1.761258,-0.358200,0.650000) {name=`test_workbench`, rot=quat(-0.9866859, 0, 0, -0.1626372)}
 
 env.clockTicking = false
 env.secondsSinceMidnight = 10*60*60
