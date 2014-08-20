bb10f81c73d9967cff29d352125d2522144e46e1
razzeeyy <razzeeyy@3c9ba142-2be6-4198-8de6-d73c80d60e27>
2014-08-20 22:27:06 +0000

    fix VerticalLine not stroking thru all screen if screen width is less than screen height
    git-svn-id: http://svn.code.sf.net/p/gritengine/code/trunk@3119 3c9ba142-2be6-4198-8de6-d73c80d60e27

--- grit_core/media/common/hud/line.lua
+++ grit_core/media/common/hud/line.lua
@@ -6,11 +6,13 @@ local Line = {
     end;
     
     parentResizedCallback = function (self, psize)
-        self.size = vec(psize.x, self.size.y);
+        --we use biggest screen side as a line length, to prevent VerticalLine shrinking
+        --(self.size.x is actually a heigh of the vertical line so sometimes the line doesn't stroke thru all screen)
+        self.size = vec(psize.x > psize.y and psize.x or psize.y, self.size.y);
     end;
     
     setThickness = function (self, width)
-        self.size = vec(self.size.x, width); 
+        self.size = vec(self.size.x, width);
     end;
     
     getThickness = function (self)
