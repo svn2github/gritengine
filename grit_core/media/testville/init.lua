1decd3a10db467006f2c550b0b8cd8202bdaf244
sparksparkspark <sparksparkspark@3c9ba142-2be6-4198-8de6-d73c80d60e27>
2015-01-02 16:17:11 +0000

    Fix testville
    git-svn-id: http://svn.code.sf.net/p/gritengine/code/trunk@3191 3c9ba142-2be6-4198-8de6-d73c80d60e27

--- grit_core/media/testville/init.lua
+++ grit_core/media/testville/init.lua
@@ -46,30 +46,34 @@ physical_material `Floor` {
     --proceduralBatches = { `TropPlant1`, `Stone` }
 }
 
-material `RoadMarkings` { }
+material `RoofTiles` { diffuseMap=`/common/tex/davec/RoofTiles_d.dds`, diffuseColour=0.5*srgb(248, 253, 255), textureScale={.25,.25} }
 
 material `Floor` { diffuseMap=`/common/tex/Test.dds`, textureScale={.25,.25} }
---material `Floor` { }
 
---material `RoadSurface` { diffuseMap=`RoadSurface_d.dds`, normalMap=`RoadSurface_n.dds`, specularMap=`RoadSurface_s.dds` }
---material `RoadSurface` { diffuseMap=`BreezeBlocks_d.dds`, normalMap=`BreezeBlocks_n.dds` }
-material `RoadSurface` { diffuseMap=`RedBrickVert_d.dds`, normalMap=`RedBrick_n.dds` }
-material `RoofTiles` { diffuseMap=`/common/tex/davec/RoofTiles_d.dds`, diffuseColour=srgb(248, 253, 255), textureScale={.25,.25} }
---material `RoadSurface` { diffuseMap=`RedBrick_d.dds`, normalMap=`RedBrick_n.dds` }
---material `RoadMarkings` { diffuseMap=`RoadMarkings_d.dds`, normalMap=`RoadMarkings_n.dds`, specularMap=`RoadMarkings_s.dds` }
-material `RoadMarkings` { overlayOffset=true, alphaReject=0.5, premultipliedAlpha=true, diffuseMap=`RoadMarkings_d.dds`, normalMap=`RoadMarkings_n.dds`, specularMap=`RoadMarkings_s.dds` }
+material `RoadSurface` { diffuseMap=`/common/tex/davec/RoadSurface_d.dds`, normalMap=`/common/tex/davec/RoadSurface_n.dds`, specularMap=`/common/tex/davec/RoadSurface_s.dds` }
+material `BreezeBlocks` { diffuseMap=`/common/tex/davec/BreezeBlocks_d.dds`, normalMap=`/common/tex/davec/BreezeBlocks_n.dds` }
+material `RedBrickVert` { diffuseMap=`/common/tex/davec/RedBrickVert_d.dds`, normalMap=`/common/tex/davec/RedBrick_n.dds` }
+material `RedBrick` { diffuseMap=`/common/tex/davec/RedBrick_d.dds`, normalMap=`/common/tex/davec/RedBrick_n.dds` }
+
+material `RoadMarkings` {
+    overlayOffset=true,
+    alphaReject=0.5,
+    premultipliedAlpha=true,
+    diffuseMap=`/common/tex/davec/RoadMarkings_d.dds`,
+    normalMap=`/common/tex/davec/RoadMarkings_n.dds`,
+    specularMap=`/common/tex/davec/RoadMarkings_s.dds`
+}
+
 class `TestFloor`   (ColClass) {receiveShadows=true,renderingDistance=1200,castShadows=true}
 class `Floor`       (ColClass) {receiveShadows=true,renderingDistance=1200,castShadows=true}
-class `FloorMarkings` (BaseClass) {gfxMesh=`Floor.mesh`, receiveShadows=true,renderingDistance=1200, materialMap={[`RoadSurface`]=`RoofTiles`}}
-class `ThunderDome` (ColClass) {receiveShadows=true,renderingDistance=400,castShadows=true}
+class `FloorMarkings` (BaseClass) {gfxMesh=`Floor.mesh`, receiveShadows=true,renderingDistance=1200, materialMap={[`RoadSurface`]=`RoadMarkings`}}
 
-
-object `Floor`       (0,0,0)   {rot=quat(1,0,0,0), name=`Floor`, materialMap={[`RoadSurface`]=`RoofTiles`}}
---object `FloorMarkings`       (0,0,0)   {rot=quat(1,0,0,0), name=`Markings`}
-object `TestFloor`   (100,0,1)   {rot=quat(1,0,0,0), name=`TestFloor`}
+object `Floor` (0,0,0) {rot=quat(1,0,0,0), name=`Floor`, aterialMap={[`RoadSurface`]=`RoadSurface`}}
+object `FloorMarkings` (0,0,0) {rot=quat(1,0,0,0), name=`Markings`}
+object `TestFloor` (100,0,1) {rot=quat(1,0,0,0), name=`TestFloor`}
 
 object `/common/props/street/Lamp` (0,0,3) { name=`lamp` }
-object `/vehicles/Evo` (0.5,-4,0.4) { name=`evo` }
+object `/vehicles/Scarman` (0.5,-4,0.4) { name=`car` }
 object `/common/props/furniture/WorkBench` (-1.761258,-0.358200,0.650000) {name=`test_workbench`, rot=quat(-0.9866859, 0, 0, -0.1626372)}
 
 env.clockTicking = false
