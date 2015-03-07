d1b924440a03839a8b5c7e294de26b9219a96280
alxspiker <alxspiker@3c9ba142-2be6-4198-8de6-d73c80d60e27>
2015-03-07 17:50:21 +0000

    Added controls to Grit menu. Rest of the controls coming soon along with key bind changing via menu.
    git-svn-id: http://svn.code.sf.net/p/gritengine/code/trunk@3251 3c9ba142-2be6-4198-8de6-d73c80d60e27

--- grit_core/media/common/hud/menu/main.lua
+++ grit_core/media/common/hud/menu/main.lua
@@ -68,9 +68,195 @@ hud_class `Main` {
                     self:setCaption(speedoStateString())
                 end
             }),
+			newButton("Controls", {
+                pressedCallback = function()
+                    self:setContent(self.controlsContent)
+                end
+            }),
             newButton("Go Back", {
                 pressedCallback = function()
                     self:setContent(self.mainContent)
+                end
+            }),
+        })
+		
+		self.controlsContent = newContent({ padding = self.padding,
+			newButton("Core Controls", {
+                pressedCallback = function()
+                    self:setContent(self.coreControlsContentPageOne)
+                end
+            }),
+			newButton("Ghost Controls", {
+                pressedCallback = function()
+                    self:setContent(self.ghostControlsContent)
+                end
+            }),
+			newButton("Drive Controls", {
+                pressedCallback = function()
+                    self:setContent(self.driveControlsContent)
+                end
+            }),
+			newButton("Foot Controls", {
+                pressedCallback = function()
+                    self:setContent(self.footControlsContent)
+                end
+            }),
+            newButton("Go Back", {
+                pressedCallback = function()
+                    self:setContent(self.optionsContent)
+                end
+            }),
+        })
+		
+		self.coreControlsContentPageOne = newContent({ padding = self.padding,  --Make a loop in the future to list all of them automatically
+			newButton("Clear Placed: "..user_core_bindings.clearPlaced, {
+                pressedCallback = function()
+                    print("Controls coming soon!")
+                end
+            }),
+			newButton("Clear Projectiles: "..user_core_bindings.clearProjectiles, {
+                pressedCallback = function()
+                    print("Controls coming soon!")
+                end
+            }),
+			newButton("Console: "..user_core_bindings.console, {
+                pressedCallback = function()
+                    print("Controls coming soon!")
+                end
+            }),
+			newButton("Game Logic Frame Step: "..user_core_bindings.gameLogicFrameStep, {
+                pressedCallback = function()
+                    print("Controls coming soon!")
+                end
+            }),
+			newButton("Game Logic Step: "..user_core_bindings.gameLogicStep, {
+                pressedCallback = function()
+                    print("Controls coming soon!")
+                end
+            }),
+			newButton("Menu: "..user_core_bindings.menu, {
+                pressedCallback = function()
+                    print("Controls coming soon!")
+                end
+            }),
+			newButton("Next Page -->", {
+                pressedCallback = function()
+                    self:setContent(self.coreControlsContentPageTwo)
+                end
+            }),
+            newButton("Go Back", {
+                pressedCallback = function()
+                    self:setContent(self.controlsContent)
+                end
+            }),
+        })
+		
+		self.coreControlsContentPageTwo = newContent({ padding = self.padding,  --Make a loop in the future to list all of them automatically
+			newButton("Physics Debug World: "..user_core_bindings.physicsDebugWorld, {
+                pressedCallback = function()
+                    print("Controls coming soon!")
+                end
+            }),
+			newButton("Physics One To One: "..user_core_bindings.physicsOneToOne, {
+                pressedCallback = function()
+                    print("Controls coming soon!")
+                end
+            }),
+			newButton("Physics Pause: "..user_core_bindings.physicsPause, {
+                pressedCallback = function()
+                    print("Controls coming soon!")
+                end
+            }),
+			newButton("Physics Split Impulse: "..user_core_bindings.physicsSplitImpulse, {
+                pressedCallback = function()
+                    print("Controls coming soon!")
+                end
+            }),
+			newButton("Physics Wire Frame: "..user_core_bindings.physicsWireFrame, {
+                pressedCallback = function()
+                    print("Controls coming soon!")
+                end
+            }),
+			newButton("Record: "..user_core_bindings.record, {
+                pressedCallback = function()
+                    print("Controls coming soon!")
+                end
+            }),
+			newButton("Next Page -->", {
+                pressedCallback = function()
+                    self:setContent(self.coreControlsContentPageThree)
+                end
+            }),
+            newButton("Go Back", {
+                pressedCallback = function()
+                    self:setContent(self.coreControlsContentPageOne)
+                end
+            }),
+        })
+		
+		self.coreControlsContentPageThree = newContent({ padding = self.padding,  --Make a loop in the future to list all of them automatically
+			newButton("Screen Shot: "..user_core_bindings.screenShot, {
+                pressedCallback = function()
+                    print("Controls coming soon!")
+                end
+            }),
+			newButton("Toggle Fullscreen: "..user_core_bindings.toggleFullScreen, {
+                pressedCallback = function()
+                    print("Controls coming soon!")
+                end
+            }),
+			newButton("Toggle VSync: "..user_core_bindings.toggleVSync, {
+                pressedCallback = function()
+                    print("Controls coming soon!")
+                end
+            }),
+			newButton("Wireframe: "..user_core_bindings.wireFrame, {
+                pressedCallback = function()
+                    print("Controls coming soon!")
+                end
+            }),
+            newButton("Go Back", {
+                pressedCallback = function()
+                    self:setContent(self.coreControlsContentPageTwo)
+                end
+            }),
+        })
+		
+		self.ghostControlsContent = newContent({ padding = self.padding,
+			newButton("ToDo: Loop to list controls", {
+                pressedCallback = function()
+                    print("Controls coming soon!")
+                end
+            }),
+            newButton("Go Back", {
+                pressedCallback = function()
+                    self:setContent(self.controlsContent)
+                end
+            }),
+        })
+		
+		self.driveControlsContent = newContent({ padding = self.padding,
+			newButton("ToDo: Loop to list controls", {
+                pressedCallback = function()
+                    print("Controls coming soon!")--self:setContent(self.mainContent)
+                end
+            }),
+            newButton("Go Back", {
+                pressedCallback = function()
+                    self:setContent(self.controlsContent)
+                end
+            }),
+        })
+		
+		self.footControlsContent = newContent({ padding = self.padding,
+			newButton("ToDo: Loop to list controls", {
+                pressedCallback = function()
+                    print("Controls coming soon!")--self:setContent(self.mainContent)
+                end
+            }),
+            newButton("Go Back", {
+                pressedCallback = function()
+                    self:setContent(self.controlsContent)
                 end
             }),
         })
