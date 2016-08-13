d18073801f0ce0c12ba474b8f27fcc60e6793b4b
sparksparkspark <sparksparkspark@3c9ba142-2be6-4198-8de6-d73c80d60e27>
2016-08-13 17:16:35 +0000

    Fix sponze for new material system
    git-svn-id: http://svn.code.sf.net/p/gritengine/code/trunk@3609 3c9ba142-2be6-4198-8de6-d73c80d60e27

--- sponza/defs.lua
+++ sponza/defs.lua
@@ -22,188 +22,162 @@ class `sponza_257` (ColClass) { castShadows = false; renderingDistance = 100 }
 end
 
 material `leaf` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/sponza_thorn.dds`;
-	normalMap = `textures/sponza_thorn_n.tga`;
-	specularMap = `textures/sponza_thorn_s.tga`;
-    alpha=true;
-    backfaces = true;
-    premultipliedAlpha=true;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/sponza_thorn.dds`,
+	normalMap = `textures/sponza_thorn_n.tga`,
+	glossMap = `textures/sponza_thorn_s.tga`,
+    sceneBlend = "ALPHA",
+    backfaces = true,
+    premultipliedAlpha = 1,
 }
 
 material `vase_round` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/vase_round.dds`;
-	normalMap = `textures/vase_round_n.tga`;
-	specularMap = `textures/vase_round_s.tga`;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/vase_round.dds`,
+	normalMap = `textures/vase_round_n.tga`,
+	glossMap = `textures/vase_round_s.tga`,
 }
 
 material `Material__57` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/vase_plant.dds`;
-	specularMap = `textures/vase_plant_s.tga`;
-    alpha=true;
-    backfaces = true;
-    premultipliedAlpha=true;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/vase_plant.dds`,
+	glossMap = `textures/vase_plant_s.tga`,
+    sceneBlend = "ALPHA",
+    backfaces = true,
+    premultipliedAlpha = 1,
 }
 
 material `Material__298` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/background.dds`;
-	normalMap = `textures/background_n.tga`;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/background.dds`,
+	normalMap = `textures/background_n.tga`,
 }
 
 material `16___Default` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
+    diffuseMask = vec(0.558, 0.558, 0.558),
 }
 
 material `bricks` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/spnza_bricks_a.dds`;
-	normalMap = `textures/spnza_bricks_a_n.tga`;
-	specularFromDiffuseAlpha = true;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/spnza_bricks_a.dds`,
+	normalMap = `textures/spnza_bricks_a_n.tga`,
+	-- specularFromDiffuseAlpha = true,
 }
 
 material `arch` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/sponza_arch.dds`;
-    specularFromDiffuseAlpha=true;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/sponza_arch.dds`,
+    -- specularFromDiffuseAlpha=true,
 }
 
 material `ceiling` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/sponza_ceiling_a.dds`;
-    specularFromDiffuseAlpha=true;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/sponza_ceiling_a.dds`,
+    -- specularFromDiffuseAlpha=true,
 }
 
 material `column_a` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/sponza_column_a.dds`;
-	normalMap = `textures/sponza_column_a_n.tga`;
-    specularFromDiffuseAlpha=true;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/sponza_column_a.dds`,
+	normalMap = `textures/sponza_column_a_n.tga`,
+    -- specularFromDiffuseAlpha=true,
 }
 
 material `floor` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/sponza_floor_a.dds`;
-    specularFromDiffuseAlpha=true;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/sponza_floor_a.dds`,
+    -- specularFromDiffuseAlpha=true,
 }
 
 material `column_c` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/sponza_column_c.dds`;
-	normalMap = `textures/sponza_column_c_n.tga`;
-    specularFromDiffuseAlpha=true;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/sponza_column_c.dds`,
+	normalMap = `textures/sponza_column_c_n.tga`,
+    -- specularFromDiffuseAlpha=true,
 }
 
 material `details` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/sponza_details.dds`;
-    specularFromDiffuseAlpha=true;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/sponza_details.dds`,
+    -- specularFromDiffuseAlpha=true,
 }
 
 material `column_b` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/sponza_column_b.dds`;
-	normalMap = `textures/sponza_column_b_n.tga`;
-    specularFromDiffuseAlpha=true;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/sponza_column_b.dds`,
+	normalMap = `textures/sponza_column_b_n.tga`,
+    -- specularFromDiffuseAlpha=true,
 }
 
 material `Material__47` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
+    diffuseMask = vec(0.558, 0.558, 0.558),
 }
 
 material `flagpole` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/sponza_flagpole.dds`;
-    specularFromDiffuseAlpha=true;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/sponza_flagpole.dds`,
+    -- specularFromDiffuseAlpha=true,
 }
 
 material `fabric_e` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/sponza_fabric_green.dds`;
-    specularMap = `textures/sponza_fabric_s.tga`;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/sponza_fabric_green.dds`,
+    glossMap = `textures/sponza_fabric_s.tga`,
 }
 
 material `fabric_d` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/sponza_fabric_blue.dds`;
-    specularMap = `textures/sponza_fabric_s.tga`;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/sponza_fabric_blue.dds`,
+    glossMap = `textures/sponza_fabric_s.tga`,
 }
 
 material `fabric_a` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/sponza_fabric.dds`;
-    specularMap = `textures/sponza_fabric_s.tga`;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/sponza_fabric.dds`,
+    glossMap = `textures/sponza_fabric_s.tga`,
 }
 
 material `fabric_g` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/sponza_curtain_blue.dds`;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/sponza_curtain_blue.dds`,
 }
 
 material `fabric_c` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/sponza_curtain.dds`;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/sponza_curtain.dds`,
 }
 
 material `fabric_f` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/sponza_curtain_green.dds`;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/sponza_curtain_green.dds`,
 }
 
 material `chain` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/chain_texture.dds`;
-	normalMap = `textures/chain_texture_n.tga`;
-    alpha=true;
-    backfaces = true;
-    premultipliedAlpha=true;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/chain_texture.dds`,
+	normalMap = `textures/chain_texture_n.tga`,
+    sceneBlend = "ALPHA",
+    backfaces = true,
+    premultipliedAlpha = 1,
 }
 
 material `vase_hanging` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/vase_hanging.dds`;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/vase_hanging.dds`,
 }
 
 material `vase` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/vase.dds`;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/vase.dds`,
 }
 
 material `Material__25` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/lion.dds`;
-	normalMap = `textures/lion_n.tga`;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/lion.dds`,
+	normalMap = `textures/lion_n.tga`,
 }
 
 material `roof` {
-	ambientColour = 0.5880;
-	diffuseColour = {0.5880,0.5880,0.5880};
-	diffuseMap = `textures/sponza_roof.dds`;
+    diffuseMask = vec(0.558, 0.558, 0.558),
+	diffuseMap = `textures/sponza_roof.dds`,
 }
