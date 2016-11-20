8f7cbe93b8bd81ac55b6d08b926e1711ab198be6
sparksparkspark <sparksparkspark@3c9ba142-2be6-4198-8de6-d73c80d60e27>
2016-11-20 20:37:24 +0000

    Remove quit instructions from welcome message, as we now have menus
    git-svn-id: http://svn.code.sf.net/p/gritengine/code/trunk@3682 3c9ba142-2be6-4198-8de6-d73c80d60e27

--- system/welcome_msg.lua
+++ system/welcome_msg.lua
@@ -8,8 +8,5 @@ print([[
 This is the Grit console.  In it you can type arbitrary Lua commands.
 The console is being displayed because you are in debug mode.
 You can toggle debug mode with the <tab> key.
-
-Try the following command to load a map:  include `playground/init.lua`
-Feeling trapped?  Quit with quit()
 ]])
 
