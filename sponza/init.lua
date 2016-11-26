b88040e86de316548041a14623f7f49735ceb6cd
sparksparkspark <sparksparkspark@3c9ba142-2be6-4198-8de6-d73c80d60e27>
2016-11-26 05:01:50 +0000

    Move spawnpoint further from the wall in sponza
    git-svn-id: http://svn.code.sf.net/p/gritengine/code/trunk@3702 3c9ba142-2be6-4198-8de6-d73c80d60e27

--- sponza/init.lua
+++ sponza/init.lua
@@ -5,7 +5,7 @@ local GameMode = extends_keep_existing (game_manager.gameModes['Sponza FPS'], Fi
     description = 'Sponza (First Person Demo)',
     previewImage = `GameMode.png`,
     map = `map.gmap`,
-    spawnPos = vector3(-12.52798, -4.982235, -10.6575),
+    spawnPos = vector3(-10.52798, -4.982235, -10.6575),
     spawnRot = quat(0.7013986, 0.04191646, -0.04244654, -0.7102685),
 
     playerGfxMesh = `/detached/characters/robot_med/robot_med.mesh`,
